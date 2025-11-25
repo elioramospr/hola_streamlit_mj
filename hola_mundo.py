7f5c3cee89ed3c6e4b6c515ebf051c4eefb1ce70
Elio Ramos <49961976+elioramospr@users.noreply.github.com>
2025-11-25 23:05:41 +0000

    revisado

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,5 +1,7 @@
 import streamlit as st 
+import pandas as pd 
+import matplotlib.pyplot as plt 
 
-#st.set_page_config(layout="wide")
+st.set_page_config(layout="wide")
 
-st.title("Hola Mundo")
+st.title("Hola Mundo..OK..bueno")
